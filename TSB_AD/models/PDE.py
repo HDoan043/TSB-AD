72dc13a9acc9d0b36c30dd85a25216b362c42885
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2026-07-01 01:23:45 +0700

    Refactor PDE model and related classes

--- TSB_AD/models/PDE.py
+++ TSB_AD/models/PDE.py
@@ -123,7 +123,7 @@ class Model(nn.Module):
         
         self.compressor = nn.ModuleList(
             [nn.Sequential(
-                nn.Conv1d(in_channels=channels, 2, kernel_size=4, stride=4, padding=0),
+                nn.Conv1d(in_channels=channels, out_channels = 2, kernel_size=4, stride=4, padding=0),
                 nn.GELU(),
                 nn.Conv1d(in_channels=2, out_channels=4, kernel_size=4, stride=4, padding=0),
                 nn.GELU()) \
