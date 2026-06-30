53e48e5cdfe442806a4d0d954341d7d8a2393631
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2026-06-30 23:42:51 +0700

    Update num_experts parameter in run_PDE function

--- TSB_AD/model_wrapper.py
+++ TSB_AD/model_wrapper.py
@@ -41,7 +41,7 @@ def run_Semisupervise_AD(model_name, data_train, data_test, **kwargs):
         print(error_message)
         return error_message
 
-def run_PDE(data_train, data_test, win_size=96, d_model =32, top_k=3, num_experts=2):
+def run_PDE(data_train, data_test, win_size=96, d_model =32, top_k=3, num_experts=4):
     from .models.PDE import PDE
     clf = PDE(win_size = win_size, d_model=d_model, channels=data_test.shape[1], top_k=top_k, num_experts=num_experts, lr = 1e-4, epochs = 50)
     clf.fit(data_train)
