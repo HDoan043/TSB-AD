b49467d5d26c48672c90cdf5d13b7f00a3711e76
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2026-07-01 10:12:04 +0700

    Refactor PDE model and associated classes
    Refactor PDE model and related classes for improved structure and readability. Added custom SinActivation class and updated MaskingNetwork and Model classes for better functionality.

--- TSB_AD/models/PDE.py
+++ TSB_AD/models/PDE.py
@@ -133,7 +133,7 @@ class Model(nn.Module):
         self.compressor = nn.ModuleList(
             [nn.Sequential(
                 # nn.Conv1d(in_channels=channels, out_channels = 2, kernel_size=4, stride=4, padding=0),
-                nn.Linear(win_size, d_model)),
+                nn.Linear(win_size, d_model),
                 nn.GELU()) \
                 # nn.Conv1d(in_channels=2, out_channels=4, kernel_size=4, stride=4, padding=0),
                 # nn.GELU()) \
