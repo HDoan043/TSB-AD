d2af9e286f5e62b1b0299d54ecea8fc44165e116
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2026-07-02 17:42:16 +0700

    Add learning rate and epochs parameters to run_PDE

--- TSB_AD/model_wrapper.py
+++ TSB_AD/model_wrapper.py
@@ -41,10 +41,10 @@ def run_Semisupervise_AD(model_name, data_train, data_test, **kwargs):
         print(error_message)
         return error_message
 
-def run_PDE(data_train, data_test, win_size=96, d_model =32, top_k=3, num_experts=4, lambda_pure=0.1, lambda_var=0.005):
+def run_PDE(data_train, data_test, win_size=96, d_model =32, top_k=3, num_experts=4, lambda_pure=0.1, lambda_var=0.005, lr=1e-4, epochs=50):
     from .models.PDE import PDE
     clf = PDE(win_size = win_size, d_model=d_model, channels=data_test.shape[1], top_k=top_k, 
-              num_experts=num_experts, lambda_pure=0.1, lambda_var=0.005, lr = 1e-4, epochs = 50)
+              num_experts=num_experts, lambda_pure=lambda_pure, lambda_var=lambda_var, lr = lr, epochs = epochs)
     clf.fit(data_train)
     score = clf.decision_function(data_test)
     return score.ravel()
